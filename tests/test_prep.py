b9ef5253edc5f7e1c939fb7d082a0b3f5c11b509
Muremwa <danmburu254@gmail.com>
2022-05-16 18:09:01 +0300

    adds new number codes for conversion
    Signed-off-by: Muremwa <danmburu254@gmail.com>

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -1,11 +1,12 @@
 import unittest
 
-from prep import number_systems
+from prep import number_systems, number_codes
 
 
 class NumberSystemsWiki(unittest.TestCase):
     def setUp(self):
         self.nms = number_systems
+        self.nmc = number_codes
 
     def test_number_systems_82(self):
         # get back 82 number systems
@@ -20,6 +21,14 @@ class NumberSystemsWiki(unittest.TestCase):
                 True,
                 all(type(value) == str for value in nm.values())
             )
+
+    def test_number_codes_length(self):
+        # get 360 codes
+        self.assertEqual(len(self.nmc), 360)
+
+    def test_number_codes_work(self):
+        chr_s = list(filter(lambda c_: bool(c_), [chr(c) for c in self.nmc]))
+        self.assertEqual(len(chr_s), 360)
 
 
 if __name__ == '__main__':

0614ddb7ce5aa4505f186228d193370b97f4b6f4
Muremwa <danmburu254@gmail.com>
2022-05-18 21:27:57 +0300

    updates return types
    Signed-off-by: Muremwa <danmburu254@gmail.com>

--- conversions/representations.py
+++ conversions/representations.py
@@ -7,9 +7,9 @@ from typing import List
 from prep import number_codes
 
 
-def convert_to_representation(numbers: List) -> str:
+def convert_to_representation(numbers: List[int]) -> str:
     return ''.join([chr(number_codes[num]) for num in numbers])
 
 
-def convert_from_representation(representation: str) -> List:
+def convert_from_representation(representation: str) -> List[int]:
     return [number_codes.index(ord(c)) for c in representation]

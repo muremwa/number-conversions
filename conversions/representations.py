65d88ae4e5323abd3a212ee1a632857395071171
Muremwa <danmburu254@gmail.com>
2022-05-18 21:50:09 +0300

    updates docs
    Signed-off-by: Muremwa <danmburu254@gmail.com>

--- conversions/representations.py
+++ conversions/representations.py
@@ -8,8 +8,10 @@ from prep import number_codes
 
 
 def convert_to_representation(numbers: List[int]) -> str:
+    """Represent the numbers using the format of one character per number"""
     return ''.join([chr(number_codes[num]) for num in numbers])
 
 
 def convert_from_representation(representation: str) -> List[int]:
+    """Get a list of numbers used to make the representation"""
     return [number_codes.index(ord(c)) for c in representation]

54cbc243fa6fd0db844d36f5af3dbca63f941e86
Muremwa <danmburu254@gmail.com>
2022-05-28 01:46:13 +0300

    removes dead code
    Signed-off-by: Muremwa <danmburu254@gmail.com>

--- n_utils/__init__.py
+++ n_utils/__init__.py
@@ -1,17 +1,8 @@
 from .rec import find_recurring_pattern
 from . import assure, find_base
 
-ensure_no_base_10 = assure.ensure_no_base_10
 ensure_base_is_standard = assure.ensure_base_is_standard
-ensure_number_is_representable = assure.ensure_number_is_representable
 base_limits = assure.ensure_number_representation_is_within_base_limits
 base_find = find_base.find
 
-__all__ = [
-    'find_recurring_pattern',
-    'ensure_no_base_10',
-    'ensure_base_is_standard',
-    'ensure_number_is_representable'
-    'base_limits',
-    'base_find'
-]
+__all__ = ['find_recurring_pattern', 'ensure_base_is_standard', 'base_limits', 'base_find']
--- n_utils/assure.py
+++ n_utils/assure.py
@@ -2,15 +2,6 @@
 from prep import number_systems, number_codes
 
 
-def ensure_no_base_10(number: str) -> bool:
-    """Ensures the number presented is actually possible to be a decimal"""
-    try:
-        float(number)
-        return True
-    except ValueError:
-        return False
-
-
 def ensure_base_is_standard(base: int) -> bool:
     """
         Ensures the base entered is one of the 82 standard bases.
@@ -19,17 +10,10 @@ def ensure_base_is_standard(base: int) -> bool:
     return base in number_systems[-1]
 
 
-def ensure_number_is_representable(number: str) -> bool:
-    """Ensure that the characters in a number are among the chosen 360"""
-    # . is removed since it's not part of the representation
-    return all([ord(c) in number_codes for c in number.replace('.', '')])
-
-
 def ensure_number_representation_is_within_base_limits(number: str, base: int) -> bool:
     """
         Ensure the representations of a number are within the base character representations
         E.g., G4 cannot be base 16 as G is well beyond base 16
-        Can be used to the same effect as ensure_number_is_representable
     """
     # . is removed since it's not part of the representation
     # uses the number codes up to a certain place to ensure only character usable for that base are checked

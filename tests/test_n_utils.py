45040853159af9260b5c50b07af7aa48567b4c7a
Muremwa <danmburu254@gmail.com>
2022-05-28 22:44:23 +0300

    remove redundant files and code
    Signed-off-by: Muremwa <danmburu254@gmail.com>

--- tests/test_n_utils.py
+++ tests/test_n_utils.py
@@ -29,39 +29,6 @@ class UtilsCase(unittest.TestCase):
         self.assertFalse(n_utils.ensure_base_is_standard(214))
         self.assertFalse(n_utils.ensure_base_is_standard(359))
 
-    def test_assurances_base_10(self):
-        self.assertTrue(n_utils.ensure_no_base_10('100'))
-        self.assertTrue(n_utils.ensure_no_base_10('140'))
-        self.assertTrue(n_utils.ensure_no_base_10('121.32'))
-        self.assertTrue(n_utils.ensure_no_base_10('0.13'))
-        self.assertTrue(n_utils.ensure_no_base_10('0'))
-        self.assertTrue(n_utils.ensure_no_base_10('0.0'))
-
-        self.assertFalse(n_utils.ensure_no_base_10('S232.2e'))
-        self.assertFalse(n_utils.ensure_no_base_10('as'))
-        self.assertFalse(n_utils.ensure_no_base_10('A3'))
-        self.assertFalse(n_utils.ensure_no_base_10('0.34sdD'))
-        self.assertFalse(n_utils.ensure_no_base_10('dsd'))
-        self.assertFalse(n_utils.ensure_no_base_10('A.0'))
-
-    def test_number_representable(self):
-        self.assertTrue(n_utils.ensure_number_is_representable('121dfË'))
-        self.assertTrue(n_utils.ensure_number_is_representable('12Ʒd.fË'))
-        self.assertTrue(n_utils.ensure_number_is_representable('12ƷdfË'))
-        self.assertTrue(n_utils.ensure_number_is_representable('1.Ǧ'))
-        self.assertTrue(n_utils.ensure_number_is_representable('1Ǧ'))
-        self.assertTrue(n_utils.ensure_number_is_representable('Ë'))
-        self.assertTrue(n_utils.ensure_number_is_representable('0'))
-        self.assertTrue(n_utils.ensure_number_is_representable('0.0'))
-
-        self.assertFalse(n_utils.ensure_number_is_representable('Ўew'))
-        self.assertFalse(n_utils.ensure_number_is_representable('12.2߷'))
-        self.assertFalse(n_utils.ensure_number_is_representable('12߷'))
-        self.assertFalse(n_utils.ensure_number_is_representable('ߘsdk2'))
-        self.assertFalse(n_utils.ensure_number_is_representable('ߝiwd.23'))
-        self.assertFalse(n_utils.ensure_number_is_representable('1ߔ'))
-        self.assertFalse(n_utils.ensure_number_is_representable('asߒ3'))
-
     def test_base_limits(self):
         self.assertTrue(n_utils.base_limits('AE', 16))
         self.assertTrue(n_utils.base_limits('AG', 30))
